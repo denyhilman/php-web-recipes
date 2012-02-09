304879398b5c223171bfe6f9a9d706d44735362c
Chris Khoo <chris.khoo@gmail.com>
2012-02-09 15:04:39 +1000

    added ability for mailer to send emails

--- includes/mailer.php
+++ includes/mailer.php
@@ -1,7 +1,7 @@
-<?php // 0.0.1
-
+<?php // 0.0.2
 /*
-Learn how to secure your emails from injection attacks with these articles:
+Sources:
+* http://www.php.net/manual/en/function.mail.php#105661
 * http://www.damonkohler.com/2008/12/email-injection.html
 * http://stackoverflow.com/questions/6527815/can-i-avoid-crlf-injection-attacks-by-replacing-just-the-cr
 */
@@ -17,9 +17,10 @@ function mailer_errorOutIfInjected($string, $typeName) {
 	}
 }
 
-function mailer_send($to, $subject, $body, $fromName, $fromAddress, $messageContentType = 'text/plain') {
-	$timestamp = time();
+function mailer_send($to, $subject, $body, $fromName, $fromAddress, $messageContentType = 'text/plain', $attachments = array()) {
+	$hasAttachments = (sizeof($attachments) > 0);
 	$phpVersion = phpversion();
+	$timestamp = time();
 
 	mailer_errorOutIfInjected($to, 'To');
 	mailer_errorOutIfInjected($subject, 'Subject');
@@ -32,18 +33,49 @@ function mailer_send($to, $subject, $body, $fromName, $fromAddress, $messageCont
 		"Reply-To: $fromName <$fromAddress>",
 		"Message-ID: <{$timestamp}.{$fromAddress}>",
 		"X-Mailer: PHP/{$phpVersion}",
-		"MIME-Version: 1.0",
-		"Content-Type: $messageContentType; charset=utf-8",
-		"Content-Transfer-Encoding: base64"
+		"MIME-Version: 1.0"
 	);
+
+	if ($hasAttachments) {
+		$semiRand = md5($timestamp);
+		$mimeBoundary = "==Multipart_Boundary_x{$semiRand}x";
+		$headers[] = "Content-Type: multipart/mixed; boundary=\"$mimeBoundary\"";
+	} else {
+		$headers[] = "Content-Type: $messageContentType; charset=utf-8";
+		$headers[] = "Content-Transfer-Encoding: base64";
+	}
+
 	$headerString = implode("\r\n", $headers);
-	
-	$content = chunk_split(base64_encode($body));
+
+	$content = array();
+	if ($hasAttachments) {
+		$content[] = "--$mimeBoundary";
+		$content[] = "Content-Type: $messageContentType; charset=utf-8";
+		$content[] = "Content-Transfer-Encoding: base64\n"; // requires newline to separate headers & content
+		$content[] = chunk_split(base64_encode($body)) . "\n"; // requires newline to separate each boundary
+
+		foreach ($attachments as $fileName => $fileContents) {
+			$content[] = "--$mimeBoundary";
+			$fileSize = strlen($fileContents);
+			$fileContentsEncoded = chunk_split(base64_encode($fileContents));
+
+			$content[] = "Content-Type: application/octet-stream; name=\"$fileName\"";
+			$content[] = "Content-Description: $fileName";
+			$content[] = "Content-Disposition: attachment; filename=\"$fileName\"";
+			$content[] = "Content-Transfer-Encoding: base64\n"; // requires newline to separate headers & content
+			$content[] = $fileContentsEncoded . "\n"; // requires newline to separate each boundary
+		}
+    	$content[] = "--{$mime_boundary}--";
+	} else {
+		$content[] = chunk_split(base64_encode($body));
+	}
+
+	$contentString = implode("\n", $content);
 
 	// on some servers, the PHP interpreter uses the sendmail_from INI setting regardless of the headers set, so we'll set it here
 	ini_set('sendmail_from', $fromAddress);
 
-	$isSent = mail($to, $subject, $content, $headerString);
+	$isSent = mail($to, $subject, $contentString, $headerString);
 
 	ini_restore('sendmail_from');
 
